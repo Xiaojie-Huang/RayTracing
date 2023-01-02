395eb009f03cafba4a2f621202f2006d022b9d21
Xiaojie-Huang <1961895405@qq.com>
2023-01-02 14:04:12 +0800

    Renderer Class and Simple Sphere

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -5,6 +5,8 @@
 #include "Walnut/Random.h"
 #include "Walnut/Timer.h"
 
+#include "Renderer.h"
+
 using namespace Walnut;
 
 class ExampleLayer : public Walnut::Layer
@@ -27,40 +29,32 @@ public:
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
-		if(m_Image)
-			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(),(float)m_Image->GetHeight() });
+		auto image = m_Renderer.GetFinalImage();
+		if(image)
+			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(),(float)image->GetHeight() },ImVec2(0,1),ImVec2(1,0));
 
 		ImGui::End();
 		ImGui::PopStyleVar();
+
+		Render();
 	}
 
 	void Render()
 	{
 		Timer timer;
 
-		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
-		{
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight,ImageFormat::RGBA);
-			delete[] m_ImageData;
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
+		//Renderer Resize
+		m_Renderer.OnResize(m_ViewportWidth,m_ViewportHeight);
 
-		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; ++i)
-		{
-			//从右往左RGBA，每8位
-			m_ImageData[i] = Random::UInt();
-			//保证透明度
-			m_ImageData[i] |= 0xff000000;
-		}
+		//Renderer Render
+		m_Renderer.Render();
 
-		m_Image->SetData(m_ImageData);
 
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 
 private:
-	std::shared_ptr<Image> m_Image;
-	uint32_t* m_ImageData = nullptr;
+	Renderer m_Renderer;
 
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 
